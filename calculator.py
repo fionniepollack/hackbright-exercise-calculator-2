4555bdebaa5e54f0fecb809eb9c146a1ab5e0b66
Fionnie Pollack <fionniepollack@gmail.com>
2021-01-07 00:25:08 +0000

    Initial commit and add pseudocode

---
+++ calculator.py
@@ -0,0 +1,47 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
+
+
+# repeat forever:
+while True:
+#   read input
+    user_input = input("What is your equation? ")
+
+#   tokenize input
+    tokens = user_input.split(',')
+
+#         if the first token is "q":
+        if token in tokens == "q":
+#           quit
+            print("Quit, you will now exit.")
+            
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is '+':
+#                   call the add function with the other two tokens
+            
+#             if the first token is '-':
+#                   call the subtract function with the other two tokens
+
+#             if the first token is '*':
+#                   call the multiply function with the other two tokens
+
+#             if the first token is '/':
+#                   call the divide function with the other two tokens
+            
+#             if the first token is 'square':
+#                   call the square function with the other two tokens
+
+#             if the first token is 'cube':
+#                   call the cube function with the other two tokens
+            
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+            
+#             if the first token is 'mod':
+#                   call the mod function with the other two tokens
